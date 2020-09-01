ba8c34088b51ad0fc7a9a9ed9b14f475055019c8
Pratham Gupta <prathamgupta567@gmail.com>
2020-09-01 21:16:11 +0530

    Upated ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # scrapeYTcomments
-this repo will scrape all the comments of a YouTube video, provided its link. This project doesn't use YouTube API as it requires the Credentials
+This repo will scrape all the comments of a YouTube video, provided its link. This project doesn't use YouTube API as it requires the Credentials
+
+Note: Selenium is needed here because Youtube is JavaScript rendered, which BeautifulSoup cannot deal with.
--- scraper.py
+++ scraper.py
@@ -1,25 +1,34 @@
+""" This module helps to scrape all the commets from a given YT video
+This modules uses selenium webdriver to open up the url in a chrome browser
+There are better ways of doing this i.e.e using YT API, but that would require the
+YT API key which is specific to the 
+"""
 import time
 from selenium.webdriver import Chrome
 from webdriver_manager.chrome import ChromeDriverManager
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-import pandas as pd   
+# Format the output as per the requirements.
+# import pandas as pd 
+
+# TODO: Pause the video in the starting itself, so that it doesn't buffer.
+# TODO: Open chrome in an incognito mode.
 
 data=[]
-# with Chrome(executable_path="/Users/pratham/Documents/data/chromedriver") as driver:
-# with Chrome(ChromeDriverManager().install()) as driver:
+# This code will check for the latest chrome driver, if it is already present in the cache
+# No need to download, otherwise download the latest version.
 driver = Chrome(ChromeDriverManager().install())
 wait = WebDriverWait(driver,15)
+# Open the YT video from which to gather the comments
 driver.get("https://www.youtube.com/watch?v=kuhhT_cBtFU&t=2s")
-for item in range(200): 
+for item in range(5): 
+        # This will wait till the loading of commentsis finished
         wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
-        time.sleep(15)
+        time.sleep(2)
 for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#content-text"))):
-        data.append(comment.text)
-    
-df = pd.DataFrame(data, columns=['comment'])
-df.head()
+        # data.append(comment.text)
+        print(comment.text)
+        print("\n")
