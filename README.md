24c1be868a5fb2fe41c3f8d07b44e0b02cf65618
Pratham Gupta <prathamgupta567@gmail.com>
2020-09-03 12:46:04 +0530

    Upated ReadMe

--- README.md
+++ README.md
@@ -2,3 +2,12 @@
 This repo will scrape all the comments of a YouTube video, provided its link. This project doesn't use YouTube API as it requires the Credentials
 
 Note: Selenium is needed here because Youtube is JavaScript rendered, which BeautifulSoup cannot deal with.
+
+## Workflow
+    This project helps scraping the YT comments without usimg the YT API
+
+## Usage
+    Type in the YT url from which you want to scrape the comments
+
+    Execute the python module
+    (--> python scraper.py)
--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ This modules uses selenium webdriver to open up the url in a chrome browser
 There are better ways of doing this i.e.e using YT API, but that would require the
 YT API key which is specific to the 
 """
+
 import time
 from selenium.webdriver import Chrome
 from webdriver_manager.chrome import ChromeDriverManager
@@ -16,14 +17,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # TODO: Pause the video in the starting itself, so that it doesn't buffer.
 # TODO: Open chrome in an incognito mode.
+# TODO: Close the Chrome browser.
 
 data=[]
+YT_URL = "https://www.youtube.com/watch?v=kuhhT_cBtFU&t=2s"
 # This code will check for the latest chrome driver, if it is already present in the cache
 # No need to download, otherwise download the latest version.
 driver = Chrome(ChromeDriverManager().install())
 wait = WebDriverWait(driver,15)
 # Open the YT video from which to gather the comments
-driver.get("https://www.youtube.com/watch?v=kuhhT_cBtFU&t=2s")
+driver.get(YT_URL)
 for item in range(5): 
         # This will wait till the loading of commentsis finished
         wait.until(EC.visibility_of_element_located((By.TAG_NAME, "body"))).send_keys(Keys.END)
@@ -32,3 +35,4 @@ for comment in wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, 
         # data.append(comment.text)
         print(comment.text)
         print("\n")
+
