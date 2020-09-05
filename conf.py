6f50f53bf014e38e52cc58805e9901cb4555819e
Pratham Gupta <prathamgupta567@gmail.com>
2020-09-05 19:30:35 +0530

    Added Conf file

---
+++ conf.py
@@ -0,0 +1,18 @@
+""" 
+Configuration Module for the YT scraper Module.
+This module allows us to set the following options
+1. Silent Mode: Run the script in the background
+2. Incognito: Run the script in the incognito mode
+3. Close Session: Close the browser session by default
+4. Retry: Number of times to retry if connection issue
+5. Limit Comments: Limit the number of comments to be scraped
+6. Scrape Time Interval: Scrape for a particular amount of time
+7. Browser Type: Specify the type of browser to be used
+8. Output Type: Secify where to dump/print the output
+9. Pause playback: Pause the playback to optimise the bandwidth usage
+10. Download Video Metadata: Download video data such as number of viewers, and more
+...
+
+"""
+
+pass
